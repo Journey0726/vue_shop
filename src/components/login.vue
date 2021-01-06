581cca9782c379cb982dc43e5472f5a510dccb2c
chenhui <1549120948@qq.com>
2021-01-06 17:32:32 +0800

    实现登录页面

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,49 +1,74 @@
 <template>
   <div class="login_container">
-      <div class="login_box">
-          <div class="avater_box">
-              <img src="../assets/logo.png" alt="">
-          </div>
+    <!-- 头像区域 -->
+    <div class="login_box">
+      <div class="avater_box">
+        <img src="../assets/logo.png" alt="" />
       </div>
+      <!-- 登录表单区域 -->
+      <el-form label-width="0px" class="form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-user-solid"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-lock"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btn">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'login'
-}
+  name: "login",
+};
 </script>
 
 <style  lang='less' scoped>
- .login_container{
-     background-color: #2b4b6b;
-     height: 100%;
-     display: flex;
-     align-items: center;
-     justify-content: center;
- }
- .login_box{
-     width: 450px;
-     height: 300px;
-     background-color: #fff;
-     border-radius: 3px;
- }
- .avater_box{
-     height: 130px;
-     width: 130px;
-     border:1px solid #eee;
-     border-radius: 50%;
-     padding: 10px;
-     box-shadow: 0 0 10px #ddd;
-     position: relative;
-     left: 50%;
-     transform: translate(-50%,-50%);
-     img{
-        
-         widows: 100%;
-         height: 100%;
-         border-radius: 50%;
-         background-color: #eee;
-     }
- }
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+}
+.avater_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  img {
+    widows: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.form {
+  position: relative;
+  top: -60px;
+  padding: 20px;
+}
+.btn {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
