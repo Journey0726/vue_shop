b18948d4c1f6d4894639f3d053c1c7a9a224a208
chenhui <1549120948@qq.com>
2021-01-26 20:19:39 +0800

    增加加载过程中的进度条

--- src/network/request.js
+++ src/network/request.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+//加载进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 export function request(config) {
     const instance = new axios.create({
         baseURL: 'https://www.liulongbin.top:8888/api/private/v1/',
@@ -9,6 +12,8 @@ export function request(config) {
     instance.interceptors.request.use(config => {
         //拦截后需要将拦截下来的请求数据返回发送
         config.headers.Authorization = window.sessionStorage.getItem('token')
+        //在请求拦截器中展示进度条
+        NProgress.start();
         return config;
     }, err => {
         console.log(err);
@@ -17,6 +22,8 @@ export function request(config) {
     // 响应拦截器
     instance.interceptors.response.use(res => {
         // 拦截后需要将拦截下来处理成的结果返回
+        //在响应拦截器中关闭进度条
+        NProgress.done()
         return res.data
     }, err => {
         console.log(err)
